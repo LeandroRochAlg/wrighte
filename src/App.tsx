21055085c0cca3dd0c2d50dbcd9eb9465719649c
LeandroRochAlg <leandrochaju@hotmail.com>
2024-10-14 20:50:33 -0300

    build: Adicionar MDEditor

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import MDEditor from '@uiw/react-md-editor';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState("**Hello World!**");
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container" style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
+      <MDEditor
+        value={value}
+        onChange={(val) => setValue(val || '')}
+        height="100%"
+        fullscreen={true}
+      />
+    </div>
+  );
 }
 
 export default App
