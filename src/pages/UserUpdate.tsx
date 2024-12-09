6576f57033bd614cb1ce900d85ae6c50bffcdb9a
Leandro Rocha <leandrochaju@hotmail.com>
2024-12-09 18:08:42 -0300

    fix: Corrigir nome do arquivo e cor de botão

--- src/pages/UserUpdate.tsx
+++ src/pages/UserUpdate.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from '../lib/api';
-import "../styles/UserUpdate.css";
+import "../styles/userUpdate.css";
 
 const UserUpdate: React.FC = () => {
     const [name, setName] = useState('');
